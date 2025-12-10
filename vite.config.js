0b26d7d4b79dd4393279c130a0c710936919bb86
suksunboonma-rgb <suksunboonma@gmail.com>
2025-12-10 21:09:20 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/zoneidea/', // แก้ให้ตรงกับชื่อ Repository บน GitHub
+  // เปลี่ยนจาก '/zoneidea/' เป็น '/SMS/' (ต้องพิมพ์ตัวเล็ก/ใหญ่ ให้ตรงกับชื่อ Repo เป๊ะๆ)
+  base: '/SMS/', 
 })
